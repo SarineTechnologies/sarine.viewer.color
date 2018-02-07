13b921132750748aa603134c1076a18c8a433ebb
ronishoham <roni.shoham@sarine.com>
2018-02-07 14:23:32 +0200

    change sarine.viewer path

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,7 @@ module.exports = function(grunt) {
         {
             core = 
             [
-                '../../core/sarine.viewer/coffee/sarine.viewer.coffee'
+                '../sarine.viewer/coffee/sarine.viewer.coffee'
             ]
 
             grunt.log.writeln("taking core files from parent folder");
