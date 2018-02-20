fa2b064e483e96259da97c023c43429fcf9488d6
Unknown <rivkas@3base.co.il>
2018-02-20 10:09:09 +0200

    add watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,7 +89,19 @@ module.exports = function(grunt) {
                 dest: config.dist.root +  '/<%= config.name %>.config',
                 src: [target + '<%= config.name %>.config']
             }
-        }
+        },
+         watch: {
+                options: {
+                    interrupt: true
+                },
+                scripts: {
+                    files: [target + 'coffee/<%= config.name %>.coffee'],
+                    tasks: ['build'],
+                    options: {
+                        spawn: false,
+                    }
+                }
+            }
     })
     grunt.registerTask('build', [
         'clean:build',
@@ -105,7 +117,8 @@ module.exports = function(grunt) {
         'copyVersion',
         'copy:bundle',
         'clean:bundlecoffee' //remove bundle.coffe file - not necessary
-    ]);
+    ])
+    grunt.registerTask('dev', ['build', 'watch']);
 
     grunt.registerTask('copyVersion' , 'copy version from package.json to sarine.viewer.clarity.config' , function (){
         var packageFile = grunt.file.readJSON(target + 'package.json');
